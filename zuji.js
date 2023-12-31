b11ad6cff06c4e0ece4f4cd318adc52fedc422dc
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-01-01 03:49:58 +0800

    更新 zuji.js

--- zuji.js
+++ zuji.js
@@ -13,16 +13,9 @@
 [mitm] 
 
 hostname = footprint-api.quthing.com
-*******************************
-Surge
-
-[Script]
-^http[s]?:\/\/footprint-api.quthing.com\/vip\/state.+$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/xiaowendong1/iosjs/xiaowendong1/zuji.js
-
-mitm]
-
-hostname = footprint-api.quthing.com
 *******************************/
+
+
 
 var body = JSON.parse($response.body);
     body.data. validVip= true,
