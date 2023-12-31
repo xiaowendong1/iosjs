f18001f581eb4c25204afcb8620d4b54f8cb3e79
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-01-01 04:08:15 +0800

    更新 pingduoduo.js

---
+++ pingduoduo.js
@@ -0,0 +1,26 @@
+/*******************************
+  公众号:木木IOS分享
+关注了解更多新科技！！！
+
+脚本名称:
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+[rewrite_local]
+^http[s]?:\/\/api.pinduoduo.com\/api\/alexa\/cells\/hub\/v3.+$ url script-response-body pinduoduo.js
+[mitm] 
+hostname = *.pinduoduo.*
+*******************************
+Surge
+
+[Script]
+^http[s]?:\/\/api.pinduoduo.com\/api\/alexa\/cells\/hub\/v3.+$ requires-body=1,max-size=0,script-path=pinduoduo.js
+
+[MITM]
+hostname = *.pinduoduo.*
+
+*******************************/
+var obj = JSON.parse($response.body);
+    obj.data.goods_name=  "人屎尿屁 ";
+    $done({body: JSON.stringify(obj)});
