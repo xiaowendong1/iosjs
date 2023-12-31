c233ddb0532522a749ed641b73110cf01a6a43ff
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-01-01 02:45:17 +0800

    更新 zuji.js

---
+++ cgingxie/zuji.js
@@ -0,0 +1,29 @@
+/*******************************
+  公众号:木木IOS分享
+关注了解更多新科技！！！
+
+脚本名称:
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+[rewrite_local]
+^http[s]?:\/\/footprint-api.quthing.com\/vip\/state.+$ url script-response-body quthing.js
+[mitm] 
+hostname = *.quthing.*
+*******************************
+Surge
+
+[Script]
+^http[s]?:\/\/footprint-api.quthing.com\/vip\/state.+$ requires-body=1,max-size=0,script-path=quthing.js
+
+[MITM]
+hostname = *.quthing.*
+
+*******************************/
+var obj = JSON.parse($response.body);
+    obj.data. validVip= true;
+obj.data.vipType= 1;
+obj.data.trialPeriod= true;
+obj.data. expireTime= 4100335752,;
+    $done({body: JSON.stringify(obj)});
