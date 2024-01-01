914a1494bf7df376f271d0830b526b44f70d3b94
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-01-02 04:34:18 +0800

    更新 tianyancam.js

---
+++ cgingxie/tianyancam.js
@@ -0,0 +1,27 @@
+/*******************************
+  公众号:木木IOS分享
+关注了解更多新科技！！！
+甜颜相机vip滤镜使用
+脚本名称:甜颜相机vip滤镜使用
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+[rewrite_local]
+^http[s]?:\/\/api.tianyancam.com\/v1\/filter\/overview.+$ url script-response-body tianyancam.js
+[mitm] 
+hostname = http://api.tianyancam.com
+*******************************
+Surge
+
+[Script]
+^http[s]?:\/\/api.tianyancam.com\/v1\/filter\/overview.+$ requires-body=1,max-size=0,script-path=tianyancam.js
+
+[MITM]
+hostname = http://api.tianyancam.com
+*******************************/
+var obj = JSON.parse($response.body);
+    obj.vipType= "SHUTTER_ON";
+obj. vipContent= false;
+obj.subName= "";
+    $done({body: JSON.stringify(obj)});
